dc185520e2606ce5cc6d1485c3bbbadef5d231cf
Mohamed Halith <mohamedhalith.smh@gmail.com>
2023-08-06 16:59:56 +0530

    ok

---
+++ index.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+# class system:
+#     def __init__(self,user,password):
+#         self.user = user
+#         self.password=password
+    
+#     def auth(self):
+#         key='password123'
+#         if self.password == key:
+#             return True
+
+
+# role = input('Enter |User or Admin| : ').lower()
+# usr = ["user","admin"]
+# while role not in usr:
+#     role = input('Enter |User or Admin| only : ').lower()
+
+# pswrd =''
+# if role == "admin":
+#     pswrd = input('Enter admin password: ')
+
+# init = system(role,pswrd)
+# init.auth()
+# if init.auth == True:
+#     print('Ok')
+# else:
+#     print('wrong pswrd')
+
+
+
+
+# init = main("","");
+# init.auth()
+            
+        
+
+
+
